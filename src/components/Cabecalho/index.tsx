d03a01934a84ad37bf707d42cad1d95c17491153
monicahillman <mmhillman1@ucs.br>
2023-03-01 14:43:35 -0300

    video 1.2

---
+++ src/components/Cabecalho/index.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import logo from './assets/logo.png';
+import perfil from './assets/perfil.png';
+
+const CabecalhoEstilizado = styled.header`
+    display:flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 2em 4em
+`
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    flex-grow: .1;
+`
+
+const LinkEstilizado = styled.a`
+ color: var(--azul-escuro);
+ font-weight: 700;
+`
+
+function Cabecalho() {
+    return (
+        <CabecalhoEstilizado>
+            <img src={logo} alt="logo da empresa Voll" />
+            <Container>
+                <img src={perfil} alt="imagem de perfil do usuário" />
+                <LinkEstilizado href="#">Sair</LinkEstilizado>
+            </Container>
+        </CabecalhoEstilizado>
+    )
+}
+
+export default Cabecalho;
