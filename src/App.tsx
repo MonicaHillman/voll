62a830362d73621b3f3b91c6e29f015bf486d2e1
monicahillman <mmhillman1@ucs.br>
2023-03-01 14:53:47 -0300

    video 1.3

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css';
 import Cabecalho from './components/Cabecalho';
+import Container from './components/Container';
 
 function App() {
   return (
-    <Cabecalho />
+    <>
+      <Cabecalho />
+      <Container></Container>
+    </>
   );
 }
 
---
+++ src/components/Container/index.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Container = styled.section`
+background-color: var(--cinza-claro);
+margin: 4em;
+border-radius: 16px;
+padding: 2em;
+height: 100%
+`
+
+export default Container;
