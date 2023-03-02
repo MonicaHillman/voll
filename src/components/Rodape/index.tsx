5ea7596342497cbeb3f263fa963ff40b7a296d59
monicahillman <mmhillman1@ucs.br>
2023-03-02 15:36:02 -0300

    ajuste no rodapé

--- src/components/Rodape/index.tsx
+++ src/components/Rodape/index.tsx
@@ -1,4 +1,7 @@
 import styled from 'styled-components';
+import whatsapp from './assets/whatsapp.png';
+import instagram from './assets/instagram.png';
+import google from './assets/google.png';
 import facebook from './assets/facebook.png';
 
 const RodapeEstilizado = styled.footer`
@@ -9,6 +12,7 @@ background-color: var(--azul-escuro);
 text-align: center;
 `
 
+
 const ListaEstilizada = styled.ul`
 display: flex;
 justify-content: space-around;
@@ -16,9 +20,11 @@ width: 10%;
 margin: 1em auto;
 `
 
+
 const ItemEstilizado = styled.li`
 list-style-type: none;
 `
+
 
 function Rodape() {
     return (
@@ -29,10 +35,26 @@ function Rodape() {
                         <img src={facebook} alt="logo do facebook" />
                     </a>
                 </ItemEstilizado>
+                <ItemEstilizado>
+                    <a href="#">
+                        <img src={whatsapp} alt="logo do whatsapp" />
+                    </a>
+                </ItemEstilizado>
+                <ItemEstilizado>
+                    <a href="#">
+                        <img src={google} alt="logo do google" />
+                    </a>
+                </ItemEstilizado>
+                <ItemEstilizado>
+                    <a href="#">
+                        <img src={instagram} alt="logo do instagram" />
+                    </a>
+                </ItemEstilizado>
             </ListaEstilizada>
             <p>2023 © Desenvolvido por Alura | Projeto fictício sem fins comerciais.</p>
         </RodapeEstilizado>
     )
 }
+
 
 export default Rodape;
