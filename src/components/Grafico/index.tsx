05440d96b7cafbec867f29d212250118a800fb14
monicahillman <mmhillman1@ucs.br>
2023-03-03 12:48:10 -0300

    video 4.4

--- src/components/Grafico/index.tsx
+++ src/components/Grafico/index.tsx
@@ -1,8 +1,9 @@
 import { ResponsiveContainer } from "recharts";
-import { Bar } from "recharts/types/cartesian/Bar";
-import { XAxis } from "recharts/types/cartesian/XAxis";
-import { YAxis } from "recharts/types/cartesian/YAxis";
-import { BarChart } from "recharts/types/chart/BarChart";
+import { Bar } from "recharts";
+import { XAxis } from "recharts";
+import { YAxis } from "recharts";
+import { BarChart } from "recharts";
+import styled from "styled-components";
 import IConsulta from "../../types/IConsulta";
 import IProfissional from "../../types/IProfissional";
 import useDadosGrafico from "./useDadosGrafico";
@@ -17,20 +18,27 @@ interface IDados {
     consultas: number
 }
 
+const SecaoEstilizada = styled.section`
+background-color: var(--branco);
+border-radius: 16px;
+`
+
 function Grafico({ profissionais, consultas }: Props) {
     let dados: Array<IDados> = useDadosGrafico({ profissionais, consultas });
     return (
-        <ResponsiveContainer width="100%" height={350}>
-            <BarChart
-                layout="vertical"
-                data={dados}
-                margin={{ top: 25, right: 40, left: 40, bottom: 20 }}
-            >
-                <XAxis type="number"></XAxis>
-                <YAxis type="category" dataKey="nome"></YAxis>
-                <Bar dataKey="consultas" fill="#083860" barSize={30}></Bar>
-            </BarChart>
-        </ResponsiveContainer>
+        <SecaoEstilizada>
+            <ResponsiveContainer width="100%" height={350}>
+                <BarChart
+                    layout="vertical"
+                    data={dados}
+                    margin={{ top: 25, right: 40, left: 40, bottom: 20 }}
+                >
+                    <XAxis type="number"></XAxis>
+                    <YAxis type="category" dataKey="nome"></YAxis>
+                    <Bar dataKey="consultas" fill="#083860" barSize={30}></Bar>
+                </BarChart>
+            </ResponsiveContainer>
+        </SecaoEstilizada>
     )
 }
 
