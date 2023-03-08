59fafa3d71c2c3492e471ca148ee1f116a705454
monicahillman <mmhillman1@ucs.br>
2023-03-08 10:46:17 -0300

    desafio aula 05

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
 import Avaliacao from './components/Avaliacao';
+import Botao from './components/Botao';
 import Cabecalho from './components/Cabecalho';
 import Container from './components/Container';
 import Grafico from './components/Grafico';
 import Rodape from './components/Rodape';
+import Subtitulo from './components/Subtitulo';
 import Tabela from './components/Tabela';
 import Titulo from './components/Titulo';
 import useDadosConsulta from './useDadosConsulta';
@@ -22,8 +24,13 @@ function App() {
       <Cabecalho />
       <Container>
         <Titulo>Área Administrativa</Titulo>
+        <Botao>Cadastrar especialista</Botao>
+        <Titulo imagem="consulta">Consultas do Dia</Titulo>
         <Tabela consultas={consultas} />
+        <Titulo imagem="grafico">Consultas mensais por especialista</Titulo>
+        <Subtitulo>Dezembro/22</Subtitulo>
         <Grafico consultas={consultas} profissionais={profissionais} />
+        <Titulo imagem="avaliacao">Avaliações de especialistas</Titulo>
         <Avaliacao profissionais={profissionais} />
       </Container>
       <Rodape />
--- src/components/Avaliacao/index.tsx
+++ src/components/Avaliacao/index.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import IProfissional from "../../types/IProfissional";
+import Botao from "../Botao";
 import Card from "./Card";
 
 const SecaoCard = styled.section`
@@ -11,11 +12,14 @@ width: 100%;
 
 function Avaliacao({ profissionais }: { profissionais: IProfissional[] | null }) {
     return (
-        <SecaoCard>
-            {profissionais?.map((profissional) => {
-                return <Card profissional={profissional} />
-            })}
-        </SecaoCard>
+        <>
+            <SecaoCard>
+                {profissionais?.map((profissional) => {
+                    return <Card profissional={profissional} />
+                })}
+            </SecaoCard>
+            <Botao>Ver mais</Botao>
+        </>
     )
 }
 
