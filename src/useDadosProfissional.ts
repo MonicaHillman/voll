5cdf2e00edcddab629c587ae8df29837a470ca7e
monicahillman <mmhillman1@ucs.br>
2023-03-08 13:58:06 -0300

    desafio aula 02

---
+++ src/useDadosProfissional.ts
@@ -0,0 +1,10 @@
+import IProfissional from "./types/IProfissional";
+import useFetch from "./useFetch";
+
+
+const useDadosProfissional = () => {
+    return useFetch<IProfissional[]>({ url: 'profissionais' });
+}
+
+
+export default useDadosProfissional;
